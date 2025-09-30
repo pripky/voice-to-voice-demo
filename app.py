9688437f88b5608313f7eceef51e33bfe8f7cf75
pripky <asthay6677@gmail.com>
2025-09-30 18:49:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import soundfile as sf
 import numpy as np
 from st_audiorec import st_audiorec
 from pydub import AudioSegment
+from io import BytesIO
 
 from openai import OpenAI
 openai_client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
